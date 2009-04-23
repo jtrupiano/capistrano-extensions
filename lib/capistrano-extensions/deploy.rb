c9accac5199892af95c77a66b2268ba369e36493
John Trupiano <jtrupiano@gmail.com>
2009-04-22 20:46:16 -0400

    Add util::tmp::check to warn the user when :tmp_dir is getting too full (>= 10 files or >= 50MB).

--- lib/capistrano-extensions/deploy.rb
+++ lib/capistrano-extensions/deploy.rb
@@ -284,6 +284,9 @@ Capistrano::Configuration.instance(:must_exist).load do
       if $? != 0
         rollback.call
       end
+      
+      # Notify user if :tmp_dir is too large
+      util::tmp::check
     end
     
     desc <<-DESC
@@ -354,6 +357,29 @@ Capistrano::Configuration.instance(:must_exist).load do
       sync_content
     end
   end
+  
+  namespace :util do
+    
+    namespace :tmp do
+      desc "[capistrano-extensions]: Displays warning if :tmp_dir has more than 10 files or is greater than 50MB"
+      task :check do
+        #[ 5 -le "`ls -1 tmp/cap | wc -l`" ] && echo "Display Me"
+        cmd = %Q{ [ 10 -le "`ls -1 #{tmp_dir} | wc -l`" ] || [ 50 -le "`du -sh #{tmp_dir} | awk '{print int($1)}'`" ] && printf "\033[1;41m Clean up #{tmp_dir} directory \033[0m\n" && du -sh #{tmp_dir}/*  }
+        system(cmd)
+      end
+    
+      # desc "Removes all but a single backup from :tmp_dir"
+      # task :clean do
+      #   
+      # end
+      # 
+      # desc "Removes all tmp files from :tmp_dir"
+      # task :remove do
+      #   
+      # end
+    end
+  end
+  
 end
 
 def pluck_pass_str(db_config)
